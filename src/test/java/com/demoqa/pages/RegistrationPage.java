859db30c2547bdf994aba9893732224eb5e45be7
svasenkov <aanher@gmail.com>
2023-06-02 19:49:56 +0200

    added pageobjects

---
+++ src/test/java/com/demoqa/pages/RegistrationPage.java
@@ -0,0 +1,53 @@
+package com.demoqa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+//    String loginInputLocator = "#first_name"; // BAD PRACTICE
+    SelenideElement
+        firstNameInput = $("#firstName"),
+        lastNameInput = $("#lastName"),
+        userEmailInput = $("#userEmail"),
+        genderWrapper = $("#genterWrapper");
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+
+}
